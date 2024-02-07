99daca1afa1d31c3996bf6b11129126585d5b76d
coachemmy <coachemmyb@gmail.com>
2024-02-07 21:30:47 +0100

    addition of new side menu side ie products and riders

--- src/components/Layout/sidebar/utils/route.ts
+++ src/components/Layout/sidebar/utils/route.ts
@@ -3,9 +3,10 @@ import CustomersIcon from "../assets/customers"
 import InvoiceHistoryIcon from "../assets/invoiceHistory"
 import OrdersIcon from "../assets/orders"
 import balanceWithdrawalIcon from "../assets/balanceWithdrawal"
+import ProductsIcon from "../assets/userManagement"
+import RidersIcon from "../assets/userManagement"
 import UserManagementIcon from "../assets/userManagement"
-import SettingsIcon from "../assets/settings"
-import ContactsIcon from "../assets/contacts"
+
 
 export const routes = [
     { name: "Dashboard", Icon: DashboardIcon, link: "/dashboard" },
@@ -28,6 +29,16 @@ export const routes = [
         name: "Cylinder Tracking",
         Icon: balanceWithdrawalIcon,
         link: "/cylinder-tracking",
+    },
+    {
+        name: "Products",
+        Icon: ProductsIcon,
+        link: "/products",
+    },
+    {
+        name: "Riders",
+        Icon: RidersIcon,
+        link: "/riders",
     },
     {
         name: "User Management",
