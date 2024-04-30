8239ef0aab07bf3836380bc59f1b97aa50a618db
coachemmy <coachemmyb@gmail.com>
2024-04-30 22:23:11 +0100

    enhancement

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./styles/global.scss"
 import { BrowserRouter } from "react-router-dom"
 import { Notifications } from "@mantine/notifications"
 import '@mantine/notifications/styles.css';
+import { AuthContextProvider } from "@contexts/authContext.tsx"
 const theme = createTheme({
     /** Put your mantine theme override here */
 })
